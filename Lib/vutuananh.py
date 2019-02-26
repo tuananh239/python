f308ae01e659be72c0234d3de23dbd8b4b4e5003
Vu Tuan Anh <vutuananh2391997@gmail.com>
2019-02-26 23:39:16 +0700

    add homework

--- Lib/vutuananh.py
+++ Lib/vutuananh.py
@@ -22,32 +22,31 @@ def fact(a):
 def check_prime(a):
 	for i in range(2,a):
 		if a % i == 0:
-			return 0;
-	return 1;
+			return 0;		
+	return 1
 
 def sort_arr(a):
 	for i in range(0,len(a)-1):
 		for j in range(i,len(a)):
 			if a[i] > a[j]:
 				#swap(a[i],a[j])
-				temp = a[i]
-				a[i] = a[j]
-				a[j] = temp
+				#temp = a[i]
+				#a[i] = a[j]
+				#a[j] = temp
+				[a[i], a[j]] = [a[j], a[i]]
 	return a
 
 def sort_matrix(a):
 	n = 0
-	for i in range(0,len(a)-1):
-		for j in range(0,len(a[i])-1):
+	b = {}
+	for i in range(len(a)):
+		for j in range(len(a[i])):
 			b[n] = a[i][j]
 			n += 1
 	sort_arr(b)
 	n = 0
-	for i in range(0,len(a)-1):
-		for j in range(0,len(a[i])-1):
+	for i in range(len(a)):
+		for j in range(len(a[i])):
 			a[i][j] = b[n]
 			n += 1
-	return b
-I = ([[5, 6], [4, 3], [1, 2]])
-print(I)
-print(sort_matrix(I))
+	return a
--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-from Lib.math import mul
-b=10
-a=10
-(a,'+',b,'=',a+b)
-print(mul(a,b))
-type(a)
+import Lib.vutuananh as math
+a=5
+b=7
+c=[4,7,9,2,4,7,1]
+i=[[2, 3],[1, 9],[6, 8]]
+print('a =',a)
+print('b =',b)
+
+if math.check_prime(a) == 1:
+	print(a,'la so nguyen to')
+else:
+	print(a,'khong phai la so nguyen to')
+if math.check_prime(b) == 1:
+	print(b,'la so nguyen to')
+else:
+	print(b,'khong phai la so nguyen to')
+
+print(a,'+',b,'-',math.add(a,b))
+print(a,'-',b,'=',math.sub(a,b))
+print(a,'x',b,'=',math.mul(a,b))
+print(a,'/',b,'=',math.div(a,b))
+
+print('giai thua cua',a,'=',math.fact(a))
+print('giai thua cua',b,'=',math.fact(b))
+print(c)
+print('day sap xep la:', math.sort_arr(c))
+print(i)
+print('ma tran sap xep la:', math.sort_matrix(i))
